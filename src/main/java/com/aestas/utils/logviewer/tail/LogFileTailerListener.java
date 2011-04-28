c64c2c99ca400fb3c9e34265e9ca42e62c63e5ed
Luciano Fiandesio <luciano@aestasit.com>
2011-04-28 17:03:34 +0200

    - removed unused classes

--- src/main/java/com/aestas/utils/logviewer/tail/LogFileTailerListener.java
+++ src/main/java/com/aestas/utils/logviewer/tail/LogFileTailerListener.java
@@ -3,12 +3,11 @@ package com.aestas.utils.logviewer.tail;
 /**
  * Provides listener notification methods when a tailed log file is updated
  */
-public interface LogFileTailerListener
-{
-  /**
-   * A new line has been added to the tailed log file
-   * 
-   * @param line   The new line that has been added to the tailed log file
-   */
-  public void newLogFileLine( String line );
+public interface LogFileTailerListener {
+    /**
+     * A new line has been added to the tailed log file
+     *
+     * @param line The new line that has been added to the tailed log file
+     */
+    public void newLogFileLine(String line);
 }
